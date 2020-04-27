c886217f170478cf3e5a4f059e9d796b1ccd9ad2
leonardo <leonardosilva291@gmail.com>
2020-04-27 13:50:05 -0300

    make it better

--- app.js
+++ app.js
@@ -33,8 +33,8 @@ const db = require("./config/db")
 	next();
 })
 	//body PArser
-		app.use(bodyParser.urlencoded({extended: true}))
-		app.use(bodyParser.json())
+	//	app.use(bodyParser.urlencoded({extended: true}))
+	//	app.use(bodyParser.json())
 	//Handlebars
 	app.engine('handlebars', handlebars({defaultlayout: 'main'}))
 	app.set('view engine', 'handlebars')
